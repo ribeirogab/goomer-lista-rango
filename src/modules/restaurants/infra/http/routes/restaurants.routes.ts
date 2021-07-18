89a973956ab0f57658b234a2f8f67b5f05c1b1cc
ribeirogab <ribeirogabx@gmail.com>
2021-07-17 22:38:18 -0300

    feat: celebrate added (joi middleware) for ensure that all inputs are correct of restaurants routes

--- src/modules/restaurants/infra/http/routes/restaurants.routes.ts
+++ src/modules/restaurants/infra/http/routes/restaurants.routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Segments, Joi } from 'celebrate';
 import { Router } from 'express';
 
 import { RestaurantsController } from '../controllers/RestaurantsController';
@@ -5,14 +6,96 @@ import { RestaurantsController } from '../controllers/RestaurantsController';
 const restaurantsRouter = Router();
 const restaurantsController = new RestaurantsController();
 
-restaurantsRouter.post('/', restaurantsController.create);
+const weekDayScheduleValidation = Joi.object().keys({
+  startHour: Joi.string().required(),
+  finishHour: Joi.string().required(),
+});
 
-restaurantsRouter.get('/', restaurantsController.index);
+const arrayUniqueAddressValidation = Joi.array()
+  .items({
+    postalCode: Joi.string().required(),
+    number: Joi.number().required(),
+  })
+  .unique((a, b) => a.postalCode === b.postalCode && a.number === b.number)
+  .min(1);
 
-restaurantsRouter.get('/:restaurantId', restaurantsController.show);
+restaurantsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      addresses: arrayUniqueAddressValidation.required(),
+      workSchedules: Joi.object().keys({
+        sunday: weekDayScheduleValidation,
+        monday: weekDayScheduleValidation,
+        tuesday: weekDayScheduleValidation,
+        wednesday: weekDayScheduleValidation,
+        thursday: weekDayScheduleValidation,
+        friday: weekDayScheduleValidation,
+        saturday: weekDayScheduleValidation,
+      }),
+    },
+  }),
+  restaurantsController.create,
+);
 
-restaurantsRouter.put('/:restaurantId', restaurantsController.update);
+restaurantsRouter.get(
+  '/',
+  celebrate({
+    [Segments.QUERY]: {
+      page: Joi.number().min(1),
+      perPage: Joi.number().min(1).max(50),
+    },
+  }),
+  restaurantsController.index,
+);
 
-restaurantsRouter.delete('/:restaurantId', restaurantsController.delete);
+restaurantsRouter.get(
+  '/:restaurantId',
+  celebrate({
+    [Segments.PARAMS]: {
+      restaurantId: Joi.string().required(),
+    },
+  }),
+  restaurantsController.show,
+);
+
+restaurantsRouter.put(
+  '/:restaurantId',
+  celebrate({
+    [Segments.PARAMS]: {
+      restaurantId: Joi.string().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string(),
+      addresses: Joi.array()
+        .items({
+          postalCode: Joi.string().required(),
+          number: Joi.number().required(),
+        })
+        .min(1),
+      workSchedule: Joi.object().keys({
+        sunday: weekDayScheduleValidation,
+        monday: weekDayScheduleValidation,
+        tuesday: weekDayScheduleValidation,
+        wednesday: weekDayScheduleValidation,
+        thursday: weekDayScheduleValidation,
+        friday: weekDayScheduleValidation,
+        saturday: weekDayScheduleValidation,
+      }),
+    },
+  }),
+  restaurantsController.update,
+);
+
+restaurantsRouter.delete(
+  '/:restaurantId',
+  celebrate({
+    [Segments.PARAMS]: {
+      restaurantId: Joi.string().required(),
+    },
+  }),
+  restaurantsController.delete,
+);
 
 export { restaurantsRouter };
