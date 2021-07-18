d8469b7b83d1d1cc87be898ceb9e2a873739ca26
ribeirogab <ribeirogabx@gmail.com>
2021-07-17 23:13:29 -0300

    feat: product routes placed on shared routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import { productsRouter } from '@modules/products/infra/http/routes/products.routes';
 import { restaurantsRouter } from '@modules/restaurants/infra/http/routes/restaurants.routes';
 
 const routes = Router();
 
 routes.use('/restaurants', restaurantsRouter);
+routes.use('/products', productsRouter);
 
 export { routes };
