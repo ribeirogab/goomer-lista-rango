23b54accc7b70e6fbd8fb5e52a4d19b852f60fb1
ribeirogab <ribeirogabx@gmail.com>
2021-07-17 23:12:20 -0300

    feat: joi validation implemented to products routes for ensure that all inputs are correct

--- src/modules/products/infra/http/routes/products.routes.ts
+++ src/modules/products/infra/http/routes/products.routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Segments, Joi } from 'celebrate';
 import { Router } from 'express';
 
 import { ProductsController } from '../controllers/ProductsController';
@@ -5,14 +6,72 @@ import { ProductsController } from '../controllers/ProductsController';
 const productsRouter = Router();
 const productsController = new ProductsController();
 
-productsRouter.post('/:restaurantId', productsController.create);
+productsRouter.post(
+  '/:restaurantId',
+  celebrate({
+    [Segments.PARAMS]: {
+      restaurantId: Joi.string().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().required(),
+      category: Joi.string().required(),
+      promotion: Joi.object().keys({
+        description: Joi.string(),
+        price: Joi.number().required(),
+        startDatetime: Joi.date().required(),
+        finishDatetime: Joi.date().required(),
+      }),
+    },
+  }),
+  productsController.create,
+);
 
-productsRouter.get('/:restaurantId', productsController.index);
+productsRouter.get(
+  '/:restaurantId',
+  celebrate({
+    [Segments.PARAMS]: {
+      restaurantId: Joi.string().required(),
+    },
+    [Segments.QUERY]: {
+      page: Joi.number(),
+      perPage: Joi.number(),
+    },
+  }),
+  productsController.index,
+);
 
-productsRouter.get('/:restaurantId/:productId', productsController.show);
+productsRouter.get(
+  '/:restaurantId/:productId',
+  celebrate({
+    [Segments.PARAMS]: {
+      restaurantId: Joi.string().required(),
+      productId: Joi.string().required(),
+    },
+  }),
+  productsController.show,
+);
 
-productsRouter.put('/:restaurantId/:productId', productsController.update);
+productsRouter.put(
+  '/:restaurantId/:productId',
+  celebrate({
+    [Segments.PARAMS]: {
+      restaurantId: Joi.string().required(),
+      productId: Joi.string().required(),
+    },
+  }),
+  productsController.update,
+);
 
-productsRouter.delete('/:restaurantId/:productId', productsController.delete);
+productsRouter.delete(
+  '/:restaurantId/:productId',
+  celebrate({
+    [Segments.PARAMS]: {
+      restaurantId: Joi.string().required(),
+      productId: Joi.string().required(),
+    },
+  }),
+  productsController.delete,
+);
 
 export { productsRouter };
