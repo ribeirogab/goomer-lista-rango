5d0497d6ffebcfe0721312db265943d15f5a98e4
ribeirogab <ribeirogabx@gmail.com>
2021-07-21 12:40:26 -0300

    refactor: business rule removed from address provider and implemented in a util file

--- src/shared/container/providers/AddressProvider/fakes/FakeAddressProvider.ts
+++ src/shared/container/providers/AddressProvider/fakes/FakeAddressProvider.ts
@@ -1,49 +1,7 @@
-import { ICreateAddressesIfItDoesNotExistDTO } from '../dtos/ICreateAddressesIfItDoesNotExistDTO';
 import { IGetAddressByPostalCodeResponseDTO } from '../dtos/IGetAddressByPostalCodeDTO';
 import { IAddressProvider } from '../models/IAddressProvider';
 
 export class FakeAddressProvider implements IAddressProvider {
-  public async createAddressesIfItDoesNotExist({
-    addressesRepository,
-    addresses: informedAddresses,
-  }: ICreateAddressesIfItDoesNotExistDTO): Promise<
-    { postalCode: string; number: string }[]
-  > {
-    const addressesPromises = informedAddresses.map(async informedAddress => {
-      let address = await addressesRepository.findByPostalCode(
-        informedAddress.postalCode,
-      );
-
-      if (!address) {
-        const {
-          postalCode,
-          state,
-          city,
-          neighborhood,
-          street,
-          country,
-          countryCode,
-        } = await this.getAddressByPostalCode(informedAddress.postalCode);
-
-        address = await addressesRepository.create({
-          postalCode,
-          state,
-          city,
-          neighborhood,
-          street,
-          country,
-          countryCode,
-        });
-      }
-
-      return { postalCode: address.postalCode, number: informedAddress.number };
-    });
-
-    const addresses = await Promise.all(addressesPromises);
-
-    return addresses;
-  }
-
   public async getAddressByPostalCode(
     postalCode: string,
   ): Promise<IGetAddressByPostalCodeResponseDTO> {
